ddbe282478cfb160e5d520fa2ba94b5ef169b7cb
Nahyan Bin Khalid <nahyan.bin.khalid@gmail.com>
2017-08-25 00:05:17 +0500

    Basic Project Setup

---
+++ database/migrations/2017_08_24_181044_create_friends_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFriendsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+	    Schema::create('friends', function (Blueprint $table) {
+		    $table->integer( 'user_id' )->unsigned();
+		    $table->integer( 'friend_id' )->unsigned();
+		    $table->integer( 'status' )->unsigned();
+		    $table->timestampsTz();
+		    $table->engine = 'InnoDB';
+	    });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('friends');
+    }
+}
